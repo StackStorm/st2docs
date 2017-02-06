77cc316b248e5f52084ed1b2443c6379fd2a173f
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-02-06 08:05:19 +0000

    wrote the first publication year to the copyright description

--- docs/source/info.py
+++ docs/source/info.py
@@ -2,13 +2,15 @@
 #
 # This file contains values that differ between open-source
 # to commercial (BWC) documentation. Everything that might
-# change from one version to another in conf.py should be 
+# change from one version to another in conf.py should be
 # placed here, otherwise you WILL break the build.
+
+from datetime import datetime
 
 master_doc = 'index'
 
 project = u'StackStorm'
-copyright = u'2016, StackStorm'
+copyright = u'2014 - %s, StackStorm' % (datetime.now().strftime("%Y"))
 author = u'Brocade Communications Inc'
 
 base_url = u'http://docs.stackstorm.com/'
