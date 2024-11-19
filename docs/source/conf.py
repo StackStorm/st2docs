d3e6ee1af8b9916ead2eaac44cc86f8eaab93ca9
Carlos <nzlosh@yahoo.com>
2024-11-19 18:22:19 +0100

    Bump documentation build to use python 3.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -440,6 +440,6 @@ texinfo_documents = info.texinfo_documents
 # texinfo_no_detailmenu = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/2': None}
+#intersphinx_mapping = {'https://docs.python.org/2': None}
 
 autoclass_content = 'both'
